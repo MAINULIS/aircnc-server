b96109c48388e5f37430b1b547fb1e5c2ce1848b
Mainul Islam <mainulislam6168@gmail.com>
2024-05-01 10:15:27 +0600

    basic server setup added

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+// middleware
+// will solve deployment of cors interceptor
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9fxhf2q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+
+async function run() {
+  try {
+    const usersCollection = client.db('aircncDB').collection('users')
+    const roomsCollection = client.db('aircncDB').collection('rooms')
+    const bookingsCollection = client.db('aircncDB').collection('bookings')
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send('AirCNC Server is running..')
+})
+
+app.listen(port, () => {
+  console.log(`AirCNC is running on port ${port}`)
+})
